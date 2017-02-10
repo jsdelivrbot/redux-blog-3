74af12e9b97ebe680eee4af6692d7ac1e8f9f5b5
alexkarevoll <alex.karevoll@gmail.com>
2017-02-10 12:10:28 -0800

    add postsshow action and reducer

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,9 +1,11 @@
-import {FETCH_POSTS} from '../actions/index'
+import {FETCH_POSTS, FETCH_POST} from '../actions/index'
 
 const INITIAL_STATE = { all: [], post: null }
 
 export default function(state=INITIAL_STATE, action) {
   switch (action.type) {
+    case FETCH_POST:
+      return {...state, post: action.payload.data}
     case FETCH_POSTS:
       // make a new state by taking current state and adding the payload
       return {...state, all: action.payload.data}
