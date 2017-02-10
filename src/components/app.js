9c061575853cb889647ae6745c98a60f87a5db16
alexkarevoll <alex.karevoll@gmail.com>
2017-02-10 13:27:01 -0800

    comments

--- src/components/app.js
+++ src/components/app.js
@@ -4,6 +4,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
+        {/* App's only job is to render child components for react-router */}
         {this.props.children}
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -12,5 +12,6 @@ const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router history={browserHistory} routes={routes} />
+    {/* you could also use hashHistory here to get things past # */}
   </Provider>
   , document.querySelector('.container'));
